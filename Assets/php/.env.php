fafed36384c6a2ce9aae2651cb6d37ccd2460820
Vincent Bonnefille <public@vincent-bonnefille.fr>
2025-06-26 10:28:49 +0200

    ADD /assets/php/ ( less .env.php MYSQL Credentials )

---
+++ Assets/php/.env.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'db_host' => '127.0.0.1:3306',
+  'db_name' => 'kanboard',
+  'db_user' => 'USER',
+  'db_pass' => 'CHANGE_ME',
+];
+?>
+
---
+++ Assets/php/change_subdescription.php
@@ -0,0 +1,1 @@
+/var/www/html/kanboard/assets/php/change_subdescription.php
